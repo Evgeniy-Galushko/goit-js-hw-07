f2a183b8cf893abb1a7232950df1a2fb79e5048a
Galushko Evgeniy <yudginchik@gmail.com>
2024-08-02 20:54:34 +0300

    add file

--- js/task-1.js
+++ js/task-1.js
@@ -1,3 +1,35 @@
 'use strict';
 
 
+const items = document.querySelectorAll("#categories .item")
+function numberOfCategories(array) {
+let numbers = []
+for (const item of array) {
+  numbers.push(item)
+}
+console.log(`Namber of categories: ${numbers.length}`)
+}
+numberOfCategories(items)
+
+const itemCategoryAnimals = document.querySelector(".item .title-animals")
+console.log(`Category: ${itemCategoryAnimals.textContent}`)
+const itemElementsAnimals = document.querySelectorAll(".item .item-ul-animals li")
+function numberOfElements(array) {
+let numbers = []
+for (const item of array) {
+  numbers.push(item)
+}
+console.log(`Elements: ${numbers.length}`)
+}
+numberOfElements(itemElementsAnimals)
+
+const itemCategoryProducts = document.querySelector(".item .title-products")
+console.log(`Category: ${itemCategoryProducts.textContent}`)
+const itemElementsProducts = document.querySelectorAll(".item .item-ul-products li")
+numberOfElements(itemElementsProducts)
+
+
+const itemCategoryTechnologies = document.querySelector(".item .title-technologies")
+console.log(`Category: ${itemCategoryTechnologies.textContent}`)
+const itemElementsTechnologies = document.querySelectorAll(".item .item-ul-technologies li")
+numberOfElements(itemElementsTechnologies)
