b55b11575aab8b659dfeb1d1f763b2bc38f523fa
Galushko Evgeniy <yudginchik@gmail.com>
2024-08-07 16:53:08 +0300

    task-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,2 +1,12 @@
 'use strict'
 
+const nameLine = document.querySelector("#name-input")
+const greetingLine = document.querySelector("#name-output")
+const events = (event) => {
+  if (event.currentTarget.value !== " " && event.currentTarget.value !== "") {
+    greetingLine.textContent = event.currentTarget.value.trim()
+  } else {
+    greetingLine.textContent = "Anonymous"
+  }
+}
+nameLine.addEventListener("input", events)
