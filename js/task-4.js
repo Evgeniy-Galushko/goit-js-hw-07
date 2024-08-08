57c08aef2882dbab9797946d65ca06cd90517ba0
Galushko Evgeniy <yudginchik@gmail.com>
2024-08-08 21:23:39 +0300

    add task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,17 @@
 'use strict'
+
+
+
+const form = document.querySelector(".login-form")
+const events = (event) => {
+  event.preventDefault();
+  const forms = event.target;
+  const email = forms.elements.email.value.trim();
+  const password = forms.elements.password.value.trim()
+  if (email === "" || password === "") {
+    return console.log('All form fields must be filled in')
+  }
+  forms.reset()
+    return console.log({ email: `${email}`, password: `${password}`})
+}
+form.addEventListener("submit", events)
--- js/task-6.js
+++ js/task-6.js
@@ -5,3 +5,40 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+
+const buttonCreate = document.querySelector(".button-one")
+// console.log(buttonCreate)
+const inputs = document.querySelector("input")
+
+const nam = event => {console.log(event.currentTarget.value)}
+inputs.addEventListener("keydown", nam)
+
+// console.log(brt)
+
+const divBoxes = document.querySelector("#boxes")
+const createBoxes = (amount) => {
+  for (let i = 1; i <= amount; i++) {
+    const div = document.createElement("div")
+    if (i === 1) {
+      divBoxes.prepend(div)
+      div.style.width = "30px";
+      div.style.height = "30px";
+    } else {
+      divBoxes.append(div)
+      div.style.width = `${30 + ((i-1) * 10)}px`;
+      div.style.height = `${30 + ((i - 1) * 10)}px`;
+    }
+  div.style.backgroundColor = getRandomHexColor()
+  }
+}
+
+createBoxes(5)
+divBoxes.style.display = "flex";
+divBoxes.style.flexDirection = "row";
+divBoxes.style.gap = "25px";
+
+
+
+// createBoxes(amount)
