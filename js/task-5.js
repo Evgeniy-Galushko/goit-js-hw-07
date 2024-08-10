988c15c8a9d096bf02fa7a5557674304c623081e
Galushko Evgeniy <yudginchik@gmail.com>
2024-08-10 16:05:58 +0300

    add task-6

--- js/task-5.js
+++ js/task-5.js
@@ -4,3 +4,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+
+const button = document.querySelector(".change-color")
+const events = (event) => {
+  const divBackgroundColor = document.querySelector(".widget .color");
+  const body = document.querySelector("body")
+  const color = getRandomHexColor()
+  divBackgroundColor.style.backgroundColor = color;
+  divBackgroundColor.style.color = "#fff";
+  body.style.backgroundColor = color;
+}
+button.addEventListener("click", events)
--- js/task-6.js
+++ js/task-6.js
@@ -6,21 +6,37 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
+const buttonCreate = document.querySelector('button[data-create]')
+const buttonDestroy = document.querySelector('button[data-destroy]')
 
+/**
+ *  Добавляет новый Div (class="box") для того чтоб туда добавить коллекцию Divs. 
+ * И потом удалять ее одним нажатием на Destroy. 
+ */
+const сreationDiv = (event) => {
+  const div = document.querySelector("#boxes")
+  const divs = document.createElement('div')
+  div.prepend(divs)
+  divs.classList.add( "box" )
+}
+buttonCreate.addEventListener("click", сreationDiv)
 
-const buttonCreate = document.querySelector(".button-one")
-// console.log(buttonCreate)
-const inputs = document.querySelector("input")
-
-const nam = event => {console.log(event.currentTarget.value)}
-inputs.addEventListener("keydown", nam)
-
-// console.log(brt)
-
-const divBoxes = document.querySelector("#boxes")
-const createBoxes = (amount) => {
+/**
+ * Проверяет на соответствие количества объектов. 
+ * Очищает input
+ * Добавляет новые объекты и рандомно подкрашивает.
+ */
+const eventCreate = (event) => {
+  const input = document.querySelector("input")
+  const events = (event) => {}
+  input.addEventListener("input", events)
+  const number = input.value 
+  if (number <= 100) {
+  createBoxes(number)
+  function createBoxes(amount) {
+    const divBoxes = document.querySelector(".box")
   for (let i = 1; i <= amount; i++) {
-    const div = document.createElement("div")
+    const div = document.createElement('div')
     if (i === 1) {
       divBoxes.prepend(div)
       div.style.width = "30px";
@@ -32,13 +48,24 @@ const createBoxes = (amount) => {
     }
   div.style.backgroundColor = getRandomHexColor()
   }
+    divBoxes.style.display = "flex";
+    divBoxes.style.flexDirection = "row";
+    divBoxes.style.gap = "25px";
+  }
+  } 
+  input.value = " "
 }
+buttonCreate.addEventListener("click", eventCreate)
+  
+/**
+ * Очищает Div (class="div-boxes) от вновь созданных объектов.
+ */
+  function destroyBoxes(){
+  const divBoxes = document.querySelector("#boxes div")
+    divBoxes.remove()
+  }
+  buttonDestroy.addEventListener("click", destroyBoxes)
 
-createBoxes(5)
-divBoxes.style.display = "flex";
-divBoxes.style.flexDirection = "row";
-divBoxes.style.gap = "25px";
 
 
 
-// createBoxes(amount)
